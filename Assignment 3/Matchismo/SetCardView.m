e67f25b2df0c4b23414de633babfe5f722ef78fa
Tom K <tomas.kraina@gmail.com>
2013-03-27 15:24:57 +0100

    draw real squiggles instead of rects

--- Assignment 3/Matchismo/SetCardView.m
+++ Assignment 3/Matchismo/SetCardView.m
@@ -11,9 +11,7 @@
 @implementation SetCardView
 
 - (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-    
+{    
     // Draw card background (rounded rect)
     [self drawBackground];
     
@@ -102,13 +100,32 @@
     [self colorPath:ovalPath];
 }
 
-// TODO: draw real squiggles instead of rects
 - (void)drawSquiggleAtPoint:(CGPoint)center
 {
-    CGRect rect = CGRectMake(0, 0, SYMBOL_WIDTH_RATIO * self.bounds.size.width, SYMBOL_HEIGHT_RATIO * self.bounds.size.height);
-    UIBezierPath *squiggle = [UIBezierPath bezierPathWithRect:rect];
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    CGPoint startPoint = CGPointMake(0, self.bounds.size.height * SYMBOL_HEIGHT_RATIO * 0.7);
+    [path moveToPoint:startPoint];
     
-    [self colorPath:squiggle];
+    CGPoint controlPoint1 = CGPointMake(0, - (self.bounds.size.height * SYMBOL_HEIGHT_RATIO * 0.75));
+    CGPoint controlPoint2 = CGPointMake(self.bounds.size.width * SYMBOL_WIDTH_RATIO * 0.6, self.bounds.size.height * SYMBOL_HEIGHT_RATIO * 0.6);
+    CGPoint endPoint1 = CGPointMake(self.bounds.size.width * SYMBOL_WIDTH_RATIO * 0.8, 0);
+    [path addCurveToPoint:endPoint1 controlPoint1:controlPoint1 controlPoint2:controlPoint2];
+    
+    CGPoint controlPoint3 = CGPointMake(self.bounds.size.width * SYMBOL_WIDTH_RATIO * 0.95, - (self.bounds.size.height * SYMBOL_HEIGHT_RATIO * 0.2));
+    CGPoint endPoint2 = CGPointMake(self.bounds.size.width * SYMBOL_WIDTH_RATIO, self.bounds.size.height * SYMBOL_HEIGHT_RATIO * .3);
+    [path addQuadCurveToPoint:endPoint2 controlPoint:controlPoint3];
+
+    CGPoint controlPoint4 = CGPointMake(self.bounds.size.width * SYMBOL_WIDTH_RATIO, (self.bounds.size.height * SYMBOL_HEIGHT_RATIO * 1.75));
+    CGPoint controlPoint5 = CGPointMake(self.bounds.size.width * SYMBOL_WIDTH_RATIO * 0.4, self.bounds.size.height * SYMBOL_HEIGHT_RATIO * 0.4);
+    CGPoint endPoint3 = CGPointMake(self.bounds.size.width * SYMBOL_WIDTH_RATIO * 0.2, self.bounds.size.height * SYMBOL_HEIGHT_RATIO);
+    [path addCurveToPoint:endPoint3 controlPoint1:controlPoint4 controlPoint2:controlPoint5];
+    
+    CGPoint controlPoint6 = CGPointMake(self.bounds.size.width * SYMBOL_WIDTH_RATIO * 0.05, self.bounds.size.height * SYMBOL_HEIGHT_RATIO * 1.2);
+    [path addQuadCurveToPoint:startPoint controlPoint:controlPoint6];
+
+    [path closePath];
+    
+    [self colorPath:path];
 }
 
 - (UIColor *)UIColor
