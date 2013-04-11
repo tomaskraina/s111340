d0c44ba13bf2695db4af8adfbd0a8d093756992c
Tom K <krajeec@gmail.com>
2013-04-11 14:47:58 +0200

    implement zooming

--- Assignment 4/SPoT/PhotoDetailViewController.m
+++ Assignment 4/SPoT/PhotoDetailViewController.m
@@ -9,9 +9,11 @@
 #import "PhotoDetailViewController.h"
 #import "FlickrFetcher.h"
 
-@interface PhotoDetailViewController ()
+@interface PhotoDetailViewController () <UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIScrollView *photoScrollView;
 @property (strong, nonatomic) NSDictionary *photoInfo;
+@property (weak, nonatomic) UIImageView *photoImageView;
+@property (nonatomic, getter = isZoomed) BOOL zoomed;
 @end
 
 @implementation PhotoDetailViewController
@@ -21,7 +23,35 @@
     self.photoInfo = photoInfo;
 }
 
+#pragma mark - Properties
+
+- (void)setPhotoImageView:(UIImageView *)imageView
+{
+    if (_photoImageView != imageView) {
+        _photoImageView = imageView;
+        
+        [self.photoScrollView addSubview:imageView];
+        self.photoScrollView.contentSize = imageView.bounds.size;
+        self.photoScrollView.minimumZoomScale = [self calculateMinimumZoomScaleForImageView:imageView];
+        [self.photoScrollView zoomToRect:self.photoImageView.bounds animated:NO];
+        
+        self.zoomed = NO;
+    }
+}
+
 #pragma mark - UIViewController life cycle
+
+- (void)viewDidLayoutSubviews
+{
+    if (self.photoImageView) {
+        self.photoScrollView.minimumZoomScale = [self calculateMinimumZoomScaleForImageView:self.photoImageView];
+        
+        if (!self.isZoomed) {
+            [self.photoScrollView zoomToRect:self.photoImageView.bounds animated:YES];
+        }
+
+    }
+}
 
 - (void)viewDidAppear:(BOOL)animated
 {
@@ -30,10 +60,7 @@
     NSURL *photoUrl = [FlickrFetcher urlForPhoto:self.photoInfo format:FlickrPhotoFormatLarge];
     NSData *photoData = [NSData dataWithContentsOfURL:photoUrl];
     UIImage *photoImage = [UIImage imageWithData:photoData];
-    UIImageView *imageView = [[UIImageView alloc] initWithImage:photoImage];
-
-    [self.photoScrollView addSubview:imageView];
-    self.photoScrollView.contentSize = imageView.bounds.size;
+    self.photoImageView = [[UIImageView alloc] initWithImage:photoImage];
 }
 
 - (void)viewDidLoad
@@ -46,6 +73,28 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UIScrollViewDelegate
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.photoImageView;
+}
+
+- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(float)scale
+{
+    self.zoomed = scale != scrollView.minimumZoomScale;
+}
+
+#pragma mark - convinience methods
+
+- (CGFloat)calculateMinimumZoomScaleForImageView:(UIImageView *)imageView
+{
+    CGFloat xZoomScale = self.photoScrollView.bounds.size.width / imageView.bounds.size.width;
+    CGFloat yZoomScale = self.photoScrollView.bounds.size.height / imageView.bounds.size.height;
+    
+    return MIN(xZoomScale, yZoomScale);
 }
 
 @end
